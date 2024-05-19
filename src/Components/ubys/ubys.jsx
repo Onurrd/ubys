9b4df8f139b99f608db7926cbe3657be3fcc9729
Onur <onur.uresin@hotmail.com>
2024-05-19 21:29:35 +0300

    ubys

---
+++ src/Components/ubys/ubys.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const ubys = () => {
+  return (
+    <div className='container'>
+        <div className='header'>
+            <div className='text'>giriş yap</div>
+        </div>
+        <div className='inputs'>
+            <div className='input'>
+                <input type='text'/>
+                <input type='password'/>
+            </div>
+        </div>
+        <div className='submit-container'>
+          <div className='submit'>giriş yap</div>
+        </div>
+    </div>
+  )
+}
